6a868ac6eeba03b04050e96be83b21d3245b079f
Marcelo Cicconet <cicconet@gmail.com>
2014-02-20 15:58:25 -0500

    ...

---
+++ PAT/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PAT
+//
+//  Created by Marcelo Cicconet on 2/20/14.
+//  Copyright (c) 2014 Marcelo Cicconet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
